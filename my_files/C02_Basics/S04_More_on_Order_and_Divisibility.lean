33bce6cc0c605c790f6acd9ee09c12ebadbaefc8
Sg <suica@users.noreply.github.com>
2024-07-23 00:55:47 +0800

    feat: c2s4

--- my_files/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ my_files/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -39,17 +39,64 @@ example : min a b = min b a := by
     apply min_le_left
 
 example : max a b = max b a := by
-  sorry
+  apply le_antisymm
+  . apply max_le
+    exact le_max_right b a
+    exact le_max_left b a
+  . apply max_le
+    exact le_max_right a b
+    exact le_max_left a b
+
 example : min (min a b) c = min a (min b c) := by
-  sorry
+  apply le_antisymm
+  · apply le_min
+    apply le_trans
+    apply min_le_left
+    apply min_le_left
+    apply le_min
+    · apply le_trans
+      apply min_le_left
+      apply min_le_right
+    · apply le_trans
+      apply min_le_right
+      apply le_refl
+  · repeat
+      apply le_min
+      apply le_min
+      apply min_le_left
+      apply le_trans
+      apply min_le_right
+      apply min_le_left
+      apply le_trans
+      apply min_le_right
+      apply min_le_right
+
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  have h (a b c: ℝ): a ≤ b -> a + c ≤ b + c :=
+    fun a_1 => add_le_add_right a_1 c
+  . apply le_min
+    apply h
+    apply min_le_left
+    apply h
+    apply min_le_right
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  . apply aux
+  . have h: min (a + c) (b + c) - c ≤ min a b := by
+      conv =>
+        rhs
+        rw [← add_neg_cancel_right a c, ← add_neg_cancel_right b c]
+      apply aux
+    linarith
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  have h: |a - b + b| ≤ |a - b| + |b| := by
+    apply abs_add
+  conv at h => simp
+  linarith
 end
 
 section
@@ -66,7 +113,17 @@ example : x ∣ x ^ 2 := by
   apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  refine Nat.dvd_add ?h₁ ?h₂
+  · refine Nat.dvd_add ?h₁.h₁ ?h₁.h₂
+    · rw [mul_comm]
+      ring_nf
+      apply dvd_mul_of_dvd_left
+      apply dvd_mul_of_dvd_left
+      exact Nat.dvd_refl x
+    · apply dvd_mul_of_dvd_left
+      exact Dvd.intro_left (x.pow 0) rfl
+  · refine Dvd.dvd.pow h ?h₂.x
+    simp
 end
 
 section
@@ -78,7 +135,11 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply dvd_antisymm
+  · apply dvd_gcd
+    · exact Nat.gcd_dvd_right m n
+    · exact Nat.gcd_dvd_left m n
+  · apply dvd_gcd
+    · exact Nat.gcd_dvd_right n m
+    · exact Nat.gcd_dvd_left n m
 end
-
-
